04f8265f61b281c0f5e0b611061ea89ba8d11622
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2023-06-15 21:33:02 -0300

    Vscode statusline

--- chadrc.lua
+++ chadrc.lua
@@ -1,6 +1,14 @@
 ---@type ChadrcConfig
 local M = {}
-M.ui = { theme = "onedark" }
+M.ui = {
+  theme = "onedark",
+  statusline = {
+    theme = "vscode", -- default/vscode/vscode_colored/minimal
+  },
+  nvdash = {
+    load_on_startup = false,
+  },
+}
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 return M
