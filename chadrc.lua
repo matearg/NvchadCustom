249cdae75c9d79893df94987e5903ad4e625dc9a
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2023-07-11 21:42:28 -0300

    Test

--- chadrc.lua
+++ chadrc.lua
@@ -1,14 +1,6 @@
 ---@type ChadrcConfig
 local M = {}
-M.ui = {
-  theme = "onedark",
-  statusline = {
-    theme = "vscode", -- default/vscode/vscode_colored/minimal
-  },
-  nvdash = {
-    load_on_startup = false,
-  },
-}
+M.ui = { theme = "catppuccin" }
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 return M
--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -12,7 +12,10 @@ lspconfig.pyright.setup {
 }
 
 lspconfig.clangd.setup {
-  on_attach = on_attach,
+  on_attach = function(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
   capabilities = capabilities,
   filetypes = { "c", "cpp" },
 }
