29402b7159b6d52467edf55cd22acc7626bc49d0
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2023-08-30 22:13:18 -0300

    I dont know why it fails

--- chadrc.lua
+++ chadrc.lua
@@ -1,12 +1,17 @@
----@type ChadrcConfig
 local M = {}
+
 M.ui = {
   theme = "gruvbox",
   transparency = true,
+  statusline = {
+    theme = "vscode",
+  },
   cmp = {
     style = "atom",
   },
 }
+
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
+
 return M
